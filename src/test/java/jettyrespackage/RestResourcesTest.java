8358a7eb1b9b9c758a12d785da5e9ef4b85af1a9
maorshap <maor.shapira@logz.io>
2019-12-08 15:31:21 +0200

    Add a new Test method.

--- src/test/java/jettyrespackage/RestResourcesTest.java
+++ src/test/java/jettyrespackage/RestResourcesTest.java
@@ -10,9 +10,20 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class RestResourcesTest {
+    private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
+    private static final String NUMBER = "0123456789";
+    private static final String DATA_FOR_RANDOM_STRING = CHAR_LOWER + CHAR_UPPER + NUMBER;
+
+    private static SecureRandom random = new SecureRandom();
     private static String BASE_URL = "http://localhost:8001/bootcamp/";
 
     private static WebTarget getWebTarget() {
@@ -49,6 +60,51 @@ public class RestResourcesTest {
         assertTrue(response.getStatus() >= 200 && response.getStatus() < 300);
 
         System.out.println(response.getStatus());
+
+    }
+
+    @Test
+    public void testIndexAndSearchOfDocument() throws InterruptedException {
+
+        String generatedString = generateRandomString(4);
+        String jsonString = "{\"message\":\"" + generatedString + "\"}";
+        String agentHeaderString = "Mozilla/5.0 (Macintosh; Intel Mac OS X)";
+
+        Response indexResponse = getWebTarget().path("index")
+                .request(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.USER_AGENT, agentHeaderString)
+                .post(Entity.json(jsonString));
+
+        assertNotNull(indexResponse);
+        assertTrue(indexResponse.getStatus() >= 200 && indexResponse.getStatus() < 300);
+
+        System.out.println("The message " + generatedString + " has been indexed successfully");
+
+        Thread.sleep(1000 * 7);
+        String userAgent = "Macintosh";
+
+        Response searchResponse = getWebTarget().path("search")
+                .queryParam("message", generatedString)
+                .queryParam("User-Agent", userAgent)
+                .request(MediaType.APPLICATION_JSON)
+                .get();
+
+        assertTrue(searchResponse.getStatus() >= 200 && searchResponse.getStatus() < 300);
+
+        System.out.println("The retrived message from elasticsearch:\n" + searchResponse.readEntity(String.class));
+    }
+
+    public static String generateRandomString(int length) {
+        if (length < 1) throw new IllegalArgumentException();
+
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int rndCharAt = random.nextInt(DATA_FOR_RANDOM_STRING.length());
+            char rndChar = DATA_FOR_RANDOM_STRING.charAt(rndCharAt);
+
+            sb.append(rndChar);
+        }
+        return sb.toString();
 
     }
 
